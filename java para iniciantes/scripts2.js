6250e61c2144cabd2760ccc9354d6a0be209057a
leonardomontalvao <leonardo.araujo050@gmail.com>
2022-05-19 14:46:56 -0300

    continuação dos cursos online

---
+++ java para iniciantes/scripts2.js
@@ -0,0 +1,32 @@
+//operadores aritimeticos
+let salario = 100;
+console.log(salario + salario + salario);
+
+// incremento e decremento
+let idade = 18
+console.log(++idade);
+console.log(--idade);
+
+//operadores de atribuição
+
+let valorTecladoGamer = 100;
+valorTecladoGamer += valorTecladoGamer
+console.log(valorTecladoGamer);
+
+// operadores de igualdade 
+// igualdade estrita === valor e tipo]
+
+console.log(1 === 1);
+console.log('1' === 1);
+
+// igualde solta 
+console.log(1 == 1);
+console.log('1' == 1);
+
+// operador ternario
+
+//tem um cliente, 100 premium, comum
+
+let ponto = 150;
+let tipo = ponto > 100 ? 'premium' : 'comum'
+console.log('este cliente é : ' + tipo);
